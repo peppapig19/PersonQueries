feca4c7dbf1e3f4dce6e816ac388b2712d585e77
Lisa <lisa.guzeeva@mail.ru>
2023-08-15 22:37:23 +0300

    Add project files.

---
+++ PersonQueries/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace PersonQueries
+{
+	internal class Program
+	{
+		static readonly SqlConnection conn = new SqlConnection("Server=.\\SQLEXPRESS;Database=Test;Trusted_Connection=True;");
+		static readonly Random ran = new Random();
+
+		static bool CreateTable()
+		{
+			SqlCommand cmd = conn.CreateCommand();
+
+			try
+			{
+				conn.Open();
+
+				cmd.CommandText = "DROP TABLE IF EXISTS Person";
+				cmd.ExecuteNonQuery();
+				cmd.CommandText = "CREATE TABLE Person (fullname VARCHAR(50), dob DATE, sex CHAR(1))";
+				cmd.ExecuteNonQuery();
+
+				return true;
+			}
+			finally
+			{
+				cmd.Dispose();
+				conn.Close();
+			}
+		}
+
+		static bool InsertRow(string fullname, DateTime dob, char sex)
+		{
+			SqlCommand cmd = conn.CreateCommand();
+
+			try
+			{
+				conn.Open();
+
+				cmd.CommandText = "INSERT INTO Person VALUES (@fullname, @dob, @sex)";
+				cmd.Parameters.AddWithValue("@fullname", fullname);
+				cmd.Parameters.AddWithValue("@dob", dob.Date);
+				cmd.Parameters.AddWithValue("@sex", sex);
+				cmd.ExecuteNonQuery();
+
+				return true;
+			}
+			finally
+			{
+				cmd.Dispose();
+				conn.Close();
+			}
+		}
+
+		static bool InsertRandomBulk()
+		{
+			SqlBulkCopy loader = new SqlBulkCopy(conn);
+			loader.DestinationTableName = "Person";
+			loader.ColumnMappings.Add(0, "fullname");
+			loader.ColumnMappings.Add(1, "dob");
+			loader.ColumnMappings.Add(2, "sex");
+
+			try
+			{
+				conn.Open();
+
+				RandomReader bulk = new RandomReader(ran, 1000000);
+				loader.WriteToServer(bulk);
+				bulk = new RandomReader(ran, 100, true);
+				loader.WriteToServer(bulk);
+
+				return true;
+			}
+			finally
+			{
+				loader.Close();
+				conn.Close();
+			}
+		}
+
+		static bool FirstSelect()
+		{
+			SqlCommand cmd = conn.CreateCommand();
+
+			try
+			{
+				conn.Open();
+
+				cmd.CommandText = "SELECT *, " +
+					"DATEDIFF(YEAR, p.dob, GETDATE()) - CASE WHEN DATEADD(YEAR, DATEDIFF(YEAR, p.dob, GETDATE()), p.dob) > GETDATE() THEN 1 ELSE 0 END AS age " +
+					"FROM Person p " +
+					"WHERE EXISTS (" +
+					"SELECT base.* FROM (" +
+					"SELECT fullname, dob FROM Person GROUP BY fullname, dob HAVING COUNT (*) = 1" +
+					") base " +
+					"WHERE p.fullname = base.fullname and p.dob = base.dob)";
+
+				using (SqlDataReader reader = cmd.ExecuteReader())
+				{
+					while (reader.Read())
+					{
+						Console.WriteLine($"{reader[0]}\t{(DateTime)reader[1]:dd.MM.yyyy}\t{reader[2]}\t{reader[3]}");
+					}
+				}
+
+				return true;
+			}
+			finally
+			{
+				cmd.Dispose();
+				conn.Close();
+			}
+		}
+
+		static bool SecondSelect()
+		{
+			SqlCommand cmd = conn.CreateCommand();
+			Stopwatch stopwatch = new Stopwatch();
+
+			try
+			{
+				conn.Open();
+				cmd.CommandText = "SELECT * FROM Person WHERE fullname LIKE 'F%' AND sex = 'm'";
+				stopwatch.Start();
+
+				using (SqlDataReader reader = cmd.ExecuteReader())
+				{
+					while (reader.Read())
+					{
+						Console.WriteLine($"{reader[0]}\t{(DateTime)reader[1]:dd.MM.yyyy}\t{reader[2]}");
+					}
+				}
+
+				stopwatch.Stop();
+				Console.WriteLine($"Query executed in (ms): {stopwatch.ElapsedMilliseconds}");
+				return true;
+			}
+			finally
+			{
+				cmd.Dispose();
+				conn.Close();
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			if (args.Length == 0)
+			{
+				Console.WriteLine("No method provided.");
+				return;
+			}
+
+			try
+			{
+				switch (args[0])
+				{
+					case "1":
+						if (CreateTable()) Console.WriteLine("Table created.");
+						break;
+					case "2":
+						bool ok = InsertRow(args[1], Convert.ToDateTime(args[2]), Convert.ToChar(args[3]));
+						if (ok) Console.WriteLine("Row inserted.");
+						break;
+					case "3":
+						FirstSelect();
+						break;
+					case "4":
+						if (InsertRandomBulk()) Console.WriteLine("Bulk inserted.");
+						break;
+					case "5":
+						SecondSelect();
+						break;
+					default:
+						Console.WriteLine("No method provided.");
+						break;
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+	}
+}
---
+++ PersonQueries/RandomReader.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Data;
+using System.Text;
+
+namespace PersonQueries
+{
+	internal class RandomReader : IDataReader
+	{
+		int cur = 0;
+		string[] row;
+
+		readonly Random ran;
+		readonly bool isPredefined;
+		const string lower = "abcdefghijklmnopqrstuvwxyz";
+		const string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		const int alphabetLength = 26;
+
+		public int FieldCount { get; set; }
+
+		public RandomReader(Random ran, int fieldCount, bool isPredefined = false)
+		{
+			FieldCount = fieldCount;
+			this.ran = ran;
+			this.isPredefined = isPredefined;
+		}
+
+		public bool Read()
+		{
+			if (cur >= FieldCount) return false;
+
+			row = new string[3];
+			row[1] = GenerateDOB();
+
+			if (isPredefined)
+			{
+				row[0] = GenerateFullName('F');
+				row[2] = "m";
+			}
+			else
+			{
+				row[0] = GenerateFullName();
+				row[2] = GenerateSex();
+			}
+
+			cur++;
+			return true;
+		}
+
+		public object GetValue(int i) => row[i];
+
+		public string GenerateFullName()
+		{
+			StringBuilder fullName = new StringBuilder();
+
+			for (int i = 1; i <= 3; i++)
+			{
+				fullName.Append(upper[ran.Next(alphabetLength)]);
+				fullName.Append(lower[ran.Next(alphabetLength)]);
+				if (i != 3) fullName.Append(' ');
+			}
+
+			return fullName.ToString();
+		}
+
+		public string GenerateFullName(char startsWith)
+		{
+			StringBuilder fullName = new StringBuilder();
+
+			for (int i = 1; i <= 3; i++)
+			{
+				fullName.Append(startsWith);
+				fullName.Append(lower[ran.Next(alphabetLength)]);
+				if (i != 3) fullName.Append(' ');
+			}
+
+			return fullName.ToString();
+		}
+
+		public string GenerateDOB()
+		{
+			return new DateTime(ran.Next(1950, 2023), ran.Next(1, 13), ran.Next(1, 29)).ToString("dd.MM.yyyy");
+		}
+
+		public string GenerateSex()
+		{
+			return ran.Next(0, 2) == 0 ? "m" : "f";
+		}
+
+		public object this[int i] => throw new NotImplementedException();
+
+		public object this[string name] => throw new NotImplementedException();
+
+		public int Depth => throw new NotImplementedException();
+
+		public bool IsClosed => throw new NotImplementedException();
+
+		public int RecordsAffected => throw new NotImplementedException();
+
+		public void Close()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool GetBoolean(int i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public byte GetByte(int i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public long GetBytes(int i, long fieldOffset, byte[] buffer, int bufferoffset, int length)
+		{
+			throw new NotImplementedException();
+		}
+
+		public char GetChar(int i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public long GetChars(int i, long fieldoffset, char[] buffer, int bufferoffset, int length)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IDataReader GetData(int i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public string GetDataTypeName(int i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DateTime GetDateTime(int i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public decimal GetDecimal(int i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public double GetDouble(int i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Type GetFieldType(int i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public float GetFloat(int i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Guid GetGuid(int i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public short GetInt16(int i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int GetInt32(int i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public long GetInt64(int i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public string GetName(int i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int GetOrdinal(string name)
+		{
+			throw new NotImplementedException();
+		}
+
+		public DataTable GetSchemaTable()
+		{
+			throw new NotImplementedException();
+		}
+
+		public string GetString(int i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int GetValues(object[] values)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool IsDBNull(int i)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool NextResult()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
